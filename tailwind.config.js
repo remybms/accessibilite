e1062bf918ac4411c55937ebd67a2ff0efb4c94e
remybms <bamasremy43@gmail.com>
2024-04-18 10:20:20 +0200

    create project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'mobile': '375px',
+      'desktop': '1440px'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
